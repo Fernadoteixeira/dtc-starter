72b40ac785121308cb689f0bd3e222c37c038acc
boldfernando <62515521+Fernadoteixeira@users.noreply.github.com>
2026-08-08 13:00:19 -0300

    feat: initialize gallery-experience package entry points

--- packages/gallery-experience/src/index.js
+++ packages/gallery-experience/src/index.js
@@ -1,3 +1,3 @@
-export * from "./types/index.js";
-export * from "./components/gallery-experience.js";
-export * from "./i18n/dictionary.js";
+export * from "./types/index";
+export * from "./components/gallery-experience";
+export * from "./i18n/dictionary";
